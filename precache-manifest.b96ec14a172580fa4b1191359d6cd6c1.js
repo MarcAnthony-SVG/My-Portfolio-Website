21c257cae74656d85631a87e81b429db1afa5dbf
Marc Rodriguez <Marcanthonyrodriguez95@gmail.com>
2020-09-18 17:03:18 -0500

    Updates

---
+++ precache-manifest.b96ec14a172580fa4b1191359d6cd6c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3edd05562ae8404e567b3573ecf53b5b",
+    "url": "/My_Portfolio_Website/index.html"
+  },
+  {
+    "revision": "9eaf5c5f7977834adf80",
+    "url": "/My_Portfolio_Website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "413f24181f8014f7455e",
+    "url": "/My_Portfolio_Website/static/js/2.441ac425.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My_Portfolio_Website/static/js/2.441ac425.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eaf5c5f7977834adf80",
+    "url": "/My_Portfolio_Website/static/js/main.ee7dbae5.chunk.js"
+  },
+  {
+    "revision": "a5bbda09600cf2ef9af0",
+    "url": "/My_Portfolio_Website/static/js/runtime-main.774aa35a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/My_Portfolio_Website/static/media/logo.5d5d9eef.svg"
+  }
+]);
