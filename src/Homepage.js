5485d2105a55c58e6c9793c63dc84f1dc62c5893
Marc <marcanthonyrodriguez95@gmail.com>
2020-12-26 10:09:23 -0600

    DEC 26th Update

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,57 +1,44 @@
-import React from "react";
+import React from 'react';
 // import { Jumbotron, Button } from "react-bootstrap";
-import Title from "./components/Title/Title.jsx";
+import Title from './components/Title/Title.jsx';
 
-import Profile from "./components/Profile/Profile.jsx";
-import Experience from "./components/Experience/Experience.jsx";
+import Profile from './components/Profile/Profile.jsx';
+import Experience from './components/Experience/Experience.jsx';
 // import Projects from "./components/OldProjects/OldProjects.jsx";
-import ContactInfo from "./components/Contact/Contact.jsx";
-import ProjectsContainer from "./components/Projects/ProjectsContainer.jsx";
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Navbar from "./components/NavigationBar/NavBar.jsx";
+import ContactInfo from './components/Contact/Contact.jsx';
+import ProjectsContainer from './components/Projects/ProjectsContainer.jsx';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/NavigationBar/NavBar.jsx';
 import * as Scroll from 'react-scroll';
-import { Link, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+import {
+  Link,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+  scroller,
+} from 'react-scroll';
 
 // import "./Homepage.css";
 
 class Homepage extends React.Component {
   constructor() {
     super();
-    this.name = "Marc Rodriguez";
+    this.name = 'Marc Rodriguez';
     this.age = 25;
-    this.city = "San Antonio";
+    this.city = 'San Antonio';
   }
 
   render() {
     return (
       <div className="App">
         <Navbar />
-        <Title
-          title="Title"
-          dark={true}
-          id="Title"
-        />
-        <Profile
-          title="Profile"
-          dark={false}
-          id="Profile"
-        />
-        <Experience
-          title="Experience"
-          dark={true}
-          id="Experience"
-        />
-        <ProjectsContainer
-          title="Projects"
-          dark={false}
-          id="Projects"
-        />
-        <ContactInfo
-          title="Contact"
-          dark={true}
-          id="Contact"
-        />
-      </div> 
+        <Title title="Title" dark={true} id="Title" />
+        <Profile title="Profile" dark={false} id="Profile" />
+        <Experience title="Experience" dark={true} id="Experience" />
+        <ProjectsContainer title="Projects" dark={false} id="Projects" />
+        <ContactInfo title="Contact" dark={true} id="Contact" />
+      </div>
     );
   }
 }
--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,7 +1,116 @@
-import React from "react";
-const Background = require("./Images/Artistic-4K-Wallpaper-3840x2160.jpg");
+import React, { useRef, useState, useEffect, useCallback } from 'react';
+import { useTransition, animated } from 'react-spring';
+
+const Background = require('./Images/Artistic-4K-Wallpaper-3840x2160.jpg');
 
 function Title(props) {
+  const transforms = {
+    overflow: 'hidden',
+    width: '100%',
+    color: 'white',
+    display: 'flex',
+    justifyContent: 'flex start',
+    alignItems: 'center',
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    willChange: 'transform, opacity, height',
+    whiteSpace: 'nowrap',
+    cursor: 'pointer',
+  };
+
+  const ref = useRef([]);
+  const [items, set] = useState([]);
+  const transitions = useTransition(items, null, {
+    from: {
+      opacity: '100%',
+      height: '50%',
+      innerHeight: '100px',
+      transform: 'perspective(100%) rotateX(0deg)',
+      color: '#F8F8FF',
+    },
+    enter: [
+      { opacity: '60%', height: '100%', innerHeight: '100px' },
+      { transform: 'perspective(100%) rotateX(180deg)', color: 'silver' },
+      { transform: 'perspective(0%) rotateX(10deg)' },
+    ],
+    leave: [
+      { color: '#F5F5F5' },
+      { innerHeight: '100px' },
+      { opacity: '0%', height: 0 },
+    ],
+    update: { color: '#4682B4' },
+  });
+
+  const reset = useCallback(() => {
+    ref.current.map(clearTimeout);
+    ref.current = [];
+    set([]);
+    ref.current.push(
+      setTimeout(() => set(['Marc', 'Anthony', 'Rodriguez']), 1000)
+    );
+    ref.current.push(setTimeout(() => set(['Software', 'Engineer']), 4000));
+    ref.current.push(setTimeout(() => set(['Ready', 'For', 'Hire']), 9000));
+  }, []);
+
+  useEffect(() => void reset(), []);
+
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '730px',
+        padding: 2,
+        backgroundImage: `url(${Background})`,
+        display: 'flex',
+        alignItems: 'center',
+      }}
+    >
+      <div
+        style={{
+          width: '500px',
+          background: 'black',
+          fontFamily:
+            'apple system, BlinkMacSystemFont, avenir next, avenir, helvetica neue, helvetica, ubuntu, roboto, noto, segoe ui, arial, sans-serif',
+          background: 'transparent',
+          webkitTouchCallout: 'none',
+          webkitUserSelect: 'none',
+          khtmlUserSelect: 'none',
+          mozUserSelect: 'none',
+          msUserSelect: 'none',
+          userSelect: 'none',
+          cursor: 'default',
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        {transitions.map(({ item, props: { innerHeight, ...rest }, key }) => (
+          <animated.div
+            className="transitions-item"
+            key={key}
+            style={rest}
+            onRender={reset}
+          >
+            <animated.div
+              style={{
+                overflow: 'hidden',
+                height: innerHeight,
+                fontSize: '4em',
+                fontWeight: 'bold',
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              {item}
+            </animated.div>
+          </animated.div>
+        ))}
+      </div>
+    </div>
+  );
+}
+export default Title;
+
+/* function Title(props) {
   return (
     <div
       id="Title"
@@ -31,3 +140,4 @@ function Title(props) {
   );
 }
 export default Title;
+*/
