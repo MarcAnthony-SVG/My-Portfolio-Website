a397597672160344725dac7202bf60cc023cee0b
Marc <marcanthonyrodriguez95@gmail.com>
2021-03-01 14:43:28 -0600

    Resume

---
+++ src/Data/Resume.js
@@ -0,0 +1,35 @@
+import MarcRodriguez from 'Resume';
+
+const firstName = 'Marc';
+const lastName = 'Rodriguez';
+const age = 25;
+const email = 'marcanthonyrodriguez95@gmail.com';
+const location = 'San Antonio';
+const ocupation = 'Software Developer';
+
+function skills() {
+  return [
+    'Javascript',
+    'HTML5',
+    'CSS3',
+    'React.js',
+    'Axios',
+    'Bootstrap',
+    'Node.js',
+    'Express',
+    'MySQL',
+    'S3',
+    'D3',
+    'GraphQL',
+    'Apollo-Server',
+    'Apollo-Client',
+    'Git',
+    'npm',
+    'Webpack',
+    'Babel',
+    'yarn',
+  ];
+}
+function education() {
+  return ['2020 Galvanize'];
+}
